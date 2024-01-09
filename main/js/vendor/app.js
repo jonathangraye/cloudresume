c4fcd319934a2f1f60ec6f1bff023ce174ceb657
jonathangraye <jonathan.stoddart1@gmail.com>
2024-01-09 18:22:02 +0000

    Views Counter  Changes to be committed: 	modified:   main/css/style.css 	modified:   main/cv.html 	modified:   main/index.html 	modified:   main/js/vendor/app.js 	renamed:    site.webmanifest -> main/site.webmanifest

---
+++ main/js/vendor/app.js
@@ -0,0 +1,16 @@
+const visitorendpoint = "https://we6oyh3ijb.execute-api.eu-west-2.amazonaws.com/test/visitors"
+const visitCount = document.querySelector(".visit-count")
+
+async function updateCounter() {
+    let response = await fetch(
+        visitorendpoint, {
+        method: "POST",       
+        headers: {
+            "Content-type": "application/json",
+        },
+    });
+    let data = await response.json();
+    visitCount.innerHTML = `Views: ${data}`;
+
+}
+updateCounter();
