e47d053647a4978dea6264694ca36fce82a7f019
jonathan graye <jonathan.stoddart1@gmail.com>
2024-02-18 23:26:08 +0000

    Changed for tomorrow
    	modified:   main/css/style.css
    	modified:   main/index.html
    	modified:   main/js/vendor/app.js

--- main/js/vendor/app.js
+++ main/js/vendor/app.js
@@ -39,3 +39,18 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 } 
+
+var coll = document.getElementsByClassName("title-card");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
