4f4bfc90eaf70dc1272ceb5d84d00a5763be804a
jonathan graye <jonathan.stoddart1@gmail.com>
2024-02-25 11:01:39 +0000

    QOL Changes Changes to be committed: 	modified:   .gitignore 	modified:   main/css/style.css 	modified:   main/cv.html 	modified:   main/index.html 	modified:   main/js/vendor/app.js

--- main/js/vendor/app.js
+++ main/js/vendor/app.js
@@ -39,6 +39,7 @@ updateCounter();
 // BUTTON - not my code
 // Get the button:
 let mybutton = document.getElementById("myBtn");
+let navbar = document.getElementById("navigation");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
@@ -46,8 +47,11 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
+    navbar.style.opacity = "0.5";
   } else {
     mybutton.style.display = "none";
+    navbar.style.opacity = "1";
+
   }
 }
 
@@ -56,4 +60,3 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 } 
-
