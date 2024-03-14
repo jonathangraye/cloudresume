18e30b2fb83f8cc1caddd4f0d64b5f3a0f54b609
jonathan graye <jonathan.stoddart1@gmail.com>
2024-03-14 23:55:18 +0000

    Changes to be committed: 	modified:   main/cv.html 	new file:   main/cv/jonathanstoddart_mar_2024.pdf 	modified:   main/js/vendor/app.js

--- main/js/vendor/app.js
+++ main/js/vendor/app.js
@@ -59,4 +59,4 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-} 
+}
